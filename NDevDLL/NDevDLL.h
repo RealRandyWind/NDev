6d66a4a502b8a95977e270c60c30f8bfe226ca1f
Randy Wind <randywind@live.nl>
2018-09-18 11:53:45 +0200

    Change from UWP to Normal

--- NDevDLL/NDevDLL.h
+++ NDevDLL/NDevDLL.h
@@ -1,1 +1,16 @@
-﻿#pragma once
+#pragma once
+
+
+
+#ifdef NDEVDLL_EXPORTS
+#define NDEVDLL_EXPORTS __declspec(dllexport)
+#else
+#define NDEVDLL_EXPORTS __declspec(dllimport)
+#endif
+
+namespace NDev
+{
+		class NDEVDLL_EXPORTS CResource;
+
+		template struct NDEVDLL_EXPORTS TSequence<_FResource>;
+}
---
+++ NDevTEST/unittestNDev.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NDevTest
+{		
+	TEST_CLASS(UnitTestNDev)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
